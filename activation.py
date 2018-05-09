Fixed backprop finally omg

activation.py
@@ -1,40 +1,58 @@
 import numpy as np
 import copy
+from abc import ABC, abstractmethod
 
-class relu():
+class activation(ABC):
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
 
-    def __activate__(self, x):
+    @abstractmethod
+    def activate(self, x):
+        pass
+    
+    @abstractmethod
+    def deriv(self, x):
+        pass    
+
+class relu(activation):
+
+    def activate(self, x):
         return np.maximum(x,0)
 
-    def __deriv__(self,x):
+    def deriv(self, x):
 
         x[x <= 0] = 0
         x[x > 0] = 1
 
         return x
 
-class leaky_relu():
+class leaky_relu(activation):
 
-    def __activate__(self, x, alpha=0.01):
+    def activate(self, x, alpha=0.1):
         y = copy.deepcopy(x)
-
         x[x <= 0] = alpha
-        x[x > 0] = 1
 
         return x*y
 
-    def __deriv__(self, x, alpha=0.01):
-
+    def deriv(self, x, alpha=0.1):
+        
         x[x <= 0] = alpha
         x[x > 0] = 1
-
+        
         return x
 
 class softmax():
 
-    def __activate__(self, X):
+    def activate(self, X):
+
         exps = np.exp(X - np.max(X))
-        return exps / np.sum(exps)
+        #x = np.max(X)
+        output = exps / np.sum(exps)
+
+        return output
 
     def __deriv__(self, X):
-        return X - np.power(X, 2)
+
+        X = self.activate(X)
+
+        return np.diagflat(X) - np.dot(X, X.T)

loss.py
@@ -1,25 +1,30 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-
-class mean_squared_error():
-    def __init__(self, **kwargs):
+class loss(ABC):
+    def __init(self, **kwargs):
         self.kwargs = kwargs
 
-    def __loss__(self, y_hat, y):
+    @abstractmethod
+    def loss(self, y_hat, y):
+        pass
+
+    @abstractmethod
+    def grad(self, y_hat, y):
+        pass
+
+class mean_squared_error(loss):
+    def loss(self, y_hat, y):
         return np.sum(np.abs(np.power(y_hat-y, 2)))
 
-class cross_entropy():
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
 
-    def __loss__(self, y_hat, y):
-        '''
-        params: y_hat (predicted)
-                y (ground truth)
-        '''
+    def grad(self, y_hat, y):
+        return np.abs(y_hat - y)
 
-        #Avoid log(0)
-        y_hat[y_hat == 0.0] = (np.min(y_hat[np.nonzero(y_hat)]))
+class cross_entropy_softmax(loss):
+    def loss(self, y_hat, y):
+        print(y_hat)
+        return -np.sum(np.log(y_hat) * y)
 
-        return np.log(y_hat) * y
-
+    def grad(self, y_hat, y):
+        return y_hat - y

main.py
@@ -1,20 +1,31 @@
-from sequential import Sequential, Layer
+from sequential import Sequential
+from sklearn.preprocessing import MinMaxScaler
 import h5py
 import json
 import numpy as np
 import random
+from layers import *
+#from keras_test import main
 
 def accuracy(nn_, test_, lbs_):
     score = 0
     for x, label in zip(test_, lbs_):
         t = nn_.predict(x)
-        dummy_var = np.argmax(nn_.predict(x))+1
-        if label == np.argmax(nn_.predict(x))+1:
+        dummy_var = np.argmax(nn_.predict(x))
+        if label == np.argmax(nn_.predict(x)):
             score += 1
 
-    return score/len(test)
+    return score/len(test_)
 
+#Returns onehot labels for multiclass classification
+def onehot_labels(test_label):
+    lbls = np.array(test_label)
+    onehot = np.zeros((lbls.size, lbls.max()+1)) 
+    onehot[np.arange(lbls.size), lbls] = 1   
+    return onehot
+    
 def load_data(data_dir='data/'):
+    print(data_dir)
     data = []
     label = []
     test = []
@@ -30,6 +41,13 @@ def load_data(data_dir='data/'):
 
     return data, label, test
 
+
+def scale_data(data):
+    X = np.array(data, dtype=np.float64)
+    scaler = MinMaxScaler()
+    scaler.fit(data)
+    X = scaler.transform(data)
+    return X
 config = json.load(open('config.json', 'r'))
 
 learning_rate = config['learning_rate']
@@ -37,27 +55,37 @@ epochs = config['epochs']
 graph = config['graph']
 data_dir = config['data_path']
 
-nn = Sequential(learning_rate=learning_rate, epochs=1)
+learning_rate = 0.001
+
+nn = Sequential(learning_rate=learning_rate, epochs=3)
 
 train, label, test = load_data(data_dir)
 
-nn.add_layer(n=400, in_shape=train.shape[1])
-
-nn.add_layer(n=100)
-nn.add_layer(n=9, activation="softmax")
-nn.compile(loss="cross_entropy")
+nn.add(Dense(n=5, in_shape=train.shape[1]))
+nn.add(Dense(n=10, activation="softmax"))
+nn.compile(loss="cross_entropy_softmax")
 
 indices = list(range(len(train)))
 random.shuffle(indices)
 
-#normalize
-train = train / 255
-
+#CHANGE 1 - Using scaler instead in scale_data method
+#train = train / 255
 train = list(map(train.__getitem__, indices))
 label = list(map(label.__getitem__, indices))
-test = train[50000:]
-test_label = label[50000:]
 
-nn.fit(train[0:50000], label[0:50000])
+#test = train[50000:]
+#test_label = label[50000:]
 
-print('Accuracy is: '+str(accuracy(nn, test, test_label)))
+#CHANGE 2 - Converting data to np arrays and pass them directly rather than
+#doing it inside the fit method. (Allows rescaling to occur outside fit)
+X = scale_data(train)
+y = np.array(label, dtype=np.float64)
+
+nn.fit(X, y)
+
+print('Accuracy is: '+str(accuracy(nn, X, label)))
+
+
+#train, label, test = load_data(data_dir)
+
+#main(scale_data(train), onehot_labels(label))

sequential.py
@@ -1,52 +1,25 @@
 import numpy as np
 from activation import *
 from loss import *
+from layers import *
 
-class Layer():
-
-    def __init__(self, n=None, in_shape=None, activation="leaky_relu"):
-        assert(n is not None)
-        if activation in globals().keys():
-            self.activation = globals()[activation]()
-        else:
-            raise NotImplementedError()
-        self.n = n
-        self.W = np.random.randn(in_shape, n)*np.sqrt(1/(n-1))
-        self.b = np.array([np.random.randn()*np.sqrt(1/(n-1)) for i in
-                           range(n)])
-        self.loss = None
-#dropout implementation; add layer that shuffles indices, and removes the first
-# 40% or something. Use floor.
-        self.grad_W = np.zeros(self.W.shape)
-        self.grad_b = np.zeros(self.b.shape)
-
-    def forward(self, inp):
-        self.input = inp
-        return self.activation.__activate__(np.dot(inp, self.W)+self.b)
-
-    def backward(self, delta):
-        self.grad_W = np.atleast_2d(self.input).T.dot(np.atleast_2d(delta.T))
-        self.grad_b = delta
-
-        return delta.dot(self.W.T)*self.activation.__deriv__(self.input)
 
 class Sequential():
 
-    def __init__(self, batch_size=1, learning_rate=0.001, epochs=100):
+    def __init__(self, batch_size=50, learning_rate=0.001, epochs=100):
         self.layers = []
         self.batch_size = batch_size
         self.epochs = epochs
         self.learning_rate = learning_rate
+        self.prev_n = None
 
-    def add_layer(self, *args, **params):
-
-        if len(self.layers) != 0:
-            params["in_shape"] = self.layers[-1].n
-
-        self.layers.append(Layer(*args, **params))
-
-    def add(self, func):
-        self.layers.append(func)
+    def add(self, obj):
+        obj.create_weights(self.prev_n)
+        self.layers.append(obj)
+        try:
+            self.prev_n = obj.n
+        except AttributeError:
+            pass
 
     def forward_pass(self, inp):
         for layer in self.layers:
@@ -55,8 +28,11 @@ class Sequential():
         return output
 
     def backward(self, delta):
-        for layer in reversed(self.layers):
-            delta = layer.backward(delta)
+        for i, layer in enumerate(reversed(self.layers)):
+            if i == 0:
+                delta = layer.backward(delta, True)
+            else:
+                delta = layer.backward(delta)
 
     def update(self):
         for layer in self.layers:
@@ -69,32 +45,65 @@ class Sequential():
         else:
             raise NotImplementedError()
 
-
     def fit(self, X, y):
-        X = np.array(X, dtype=np.float64)
-        y = np.array(y, dtype=np.float64)
-
+  
+        num_classes = len(np.unique(y))
+        
         for k in range(self.epochs):
             print('Starting epoch: '+str(k))
             loss = np.zeros(X.shape[0])
+            batches = self.get_batches(X,y)
+            for i in range(len(batches)):
 
-            for i in range(X.shape[0]):
-                i = np.random.randint(X.shape[0])
-                y_hat = self.forward_pass(X[i])
+                batch_X, batch_Y = batches[i]
 
-                grd_trth = np.array([0 for j in range(int(y.max()))], dtype=np.float64)
-
-                grd_trth[int(y[i])-1] = 1
-
-                loss_ = self.loss.__loss__(y_hat, grd_trth)
+                y_hat = self.forward_pass(batch_X)
+                
+                #CHANGE 3
+                loss_ = self.loss.loss(y_hat, batch_Y)
+                
+                #CHANGE 4
                 loss[i] = np.mean(loss_)
-                delta = loss[i] * self.layers[-1].activation.__deriv__(y_hat)
+                delta = self.loss.grad(y_hat, batch_Y)
 
+                #CHANGE 5
+                if i % 1000 == 0:
+                    print(y_hat)
+                    print('Loss at stoch item %s is %s' % (i, loss[i]))
+                
                 self.backward(delta)
-
                 self.update()
-            print("Current loss:"+ str(np.mean(loss)))
+            
+            print('=====================')
+            print("Current mean loss:"+ str(np.mean(loss)))
+            
+            
     def predict(self, x):
         x = np.array(x)
 
         return self.forward_pass(x)
+
+
+    def get_batches(self, X, Y):
+        m = X.shape[0]f
+        mini_batches = []
+
+        permutation = list(np.random.permutation(m))
+
+        shuffled_X = X[permutation]
+        shuffled_Y = Y[permutation]
+
+        num_complete_minibatches = math.floor(m / self.batch_size)
+        for k in range(0, int(num_complete_minibatches)):
+            mini_batch_X = shuffled_X[(k * self.batch_size): (k + 1) * self.batch_size]
+            mini_batch_Y = shuffled_Y[(k * self.batch_size): (k + 1) * self.batch_size]
+            mini_batch = (mini_batch_X, mini_batch_Y)
+            mini_batches.append(mini_batch)
+
+        if m % batchsize != 0:
+            mini_batch_X = shuffled_X[-(m % self.batch_size): m]
+            mini_batch_Y = shuffled_Y[-(m % self.batch_size): m]
+            mini_batch = (mini_batch_X, mini_batch_Y)
+            mini_batches.append(mini_batch)
+
+        return mini_batches